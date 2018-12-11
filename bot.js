eecbf32f2adc841ed497f49c936106758f1c561a
shadowssxx <43233768+shadowssxx@users.noreply.github.com>
2018-12-11 15:36:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,6 @@ const ytdl = require('ytdl-core');
 const gif = require("gif-search");
 const googl = require('goo.gl');
 const moment = require('moment');
-var antispam = require("anti-spam");
 const devs = ['439102535693762582']; // ايدي الاشخاص اللي تريدهم يغيرون الستريم والاسم والصورة والرستارت
 var prefix = "-"; // برفكس كل الاوامر تقريباً
 const adminprefix = "-"  // برفكس الستريم وتغيير الصورة والاسم
